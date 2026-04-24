22fb9a405778a19e93658d0de5fd25d061017d91
Takanashi Ray <bergoniaraymund@gmail.com>
2026-04-25 01:21:10 +0800

    kkkk

--- RFID/__init__.py
+++ RFID/__init__.py
@@ -4,7 +4,6 @@ from RFID.superadmin.sadmin import sadmin
 from RFID.superadmin.sregister import sregister
 from RFID.superadmin.teacher import teacher_bp
 from RFID.superadmin.section import section_bp
-from RFID.user.user import rfid_bp, start_rfid_thread
 
 def create_app():
     app = Flask(
@@ -17,8 +16,6 @@ def create_app():
     app.register_blueprint(admin_bp, url_prefix='/admin')
     app.register_blueprint(sadmin, url_prefix='/superadmin')
     app.register_blueprint(sregister, url_prefix='/superadmin')
-    app.register_blueprint(rfid_bp, url_prefix='/user')
     app.register_blueprint(teacher_bp, url_prefix='/superadmin')
     app.register_blueprint(section_bp, url_prefix='/superadmin')
-    start_rfid_thread()
     return app
